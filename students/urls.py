641c5afa53bc510dc99ffb1d78b570f29b83d0ec
surasysdev <surasys@gmail.com>
2023-04-26 07:56:15 +0530

    added create delete update forms

--- students/urls.py
+++ students/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 
-from students.views import StudentsListView, StudentDetailView
+from students.views import StudentsListView, StudentDetailView, StudentCreateView, StudentDeleteView, StudentUpdateView
 
 urlpatterns = [
     path("students/", StudentsListView.as_view(), name="students_list"),
     path("student/<int:pk>/", StudentDetailView.as_view(), name="student_detail"),
+    path("student/create/", StudentCreateView.as_view(), name="student_create"),
+    path("student/<int:pk>/delete", StudentDeleteView.as_view(), name="student_delete"),
+    path("student/<int:pk>/update", StudentUpdateView.as_view(), name="student_update")
 ]
